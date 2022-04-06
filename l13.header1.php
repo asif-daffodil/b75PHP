6550c4557e1653707e5a730f9bbc7fc2c7317188
Asif Abir <abir@dipti.com.bd>
2022-04-06 15:57:36 +0600

    apr-6-2022

---
+++ l13.header1.php
@@ -0,0 +1,8 @@
+<?php  
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+       header("location: ./l13.header2.php");
+    }
+?>
+<form action="" method="post">
+    <button>Read More</button>
+</form>
---
+++ l13.header2.php
@@ -0,0 +1,10 @@
+<?php  
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        header("location: ./l13.header1.php");
+    }
+?>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos dolorum error porro neque minima culpa sed facere assumenda laborum officiis. Cupiditate, neque dicta iste quas vitae blanditiis beatae repudiandae qui ratione exercitationem delectus natus culpa aut. Consequatur, itaque! Ipsum ullam necessitatibus molestias numquam nemo, architecto laboriosam non, expedita unde culpa, eos placeat pariatur vero? Numquam quibusdam aliquid nisi omnis deleniti.
+<br><br>
+<form action="" method="post">
+    <button>Back &lArr;</button>
+</form>
